88438b10e0f6cec3c44f31a559ac6a32f15bc00b
Jamey Bryce <jybryce4@gmail.com>
2022-01-23 19:06:27 -0600

    added some comments

--- src/index.js
+++ src/index.js
@@ -1,8 +1,10 @@
 const express = require('express');
+// Will need apollo for GraphQL API
 const { ApolloServer, gql } = require('apollo-server-express');
 
 const app = express();
 
+// Basic server outline
 app.get('/', (req, res) => res.send('Hello World'));
 
 const port = process.env.PORT || 4000;
